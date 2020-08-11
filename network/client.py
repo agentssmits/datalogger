a0e020d7ab63d4c9d0a47e3e617787895b5cd99d
agent-smith <agents.smits@inbox.lv>
2020-08-11 14:40:57 +0300

    basic server/client scripts added

--- network/client.py
+++ network/client.py
@@ -1,12 +1,49 @@
 import socket
 import time
+import os
+
+from signal import signal, SIGINT
+from sys import exit, exc_info
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.connect((HOST, PORT))
-	while 1:
-		data = s.recv(1024)
-		print('Received', repr(data))
-		time.sleep(0.1)
+global s
+
+def gracefulStop(terminate = False):
+	global s
+	try:
+		s.close()
+		if terminate:
+			exit(0)
+	except Exception as e:
+		printErr(e)
+		
+def gracefulStopHandler(signal_received, frame):
+	print('SIGINT or CTRL-C detected. Exiting gracefully')
+	gracefulStop(terminate = True)
+	
+def printErr(e):
+	print("Exception: %s" % str(e))
+	print("Caused by:")
+	exc_type, exc_obj, exc_tb = exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	
+if __name__ == "__main__":
+	global s
+	signal(SIGINT, gracefulStopHandler)
+	
+	while True:
+		try:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+				s.connect((HOST, PORT))
+				while 1:
+					data = s.recv(1024).decode()
+					print('Received', data)
+					if "stop" in data:
+						gracefulStop(terminate = True)
+					time.sleep(0.1)
+		except Exception as e:
+			printErr(e)
+			gracefulStop()
--- network/server.py
+++ network/server.py
@@ -2,10 +2,37 @@ import socket
 from datetime import datetime
 import random
 import time
+import os
+
+from signal import signal, SIGINT
+from sys import exit, exc_info
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+global conn, s
+
+def gracefulStop():
+	global conn, s
+	try:
+		conn.sendall("stop".encode())
+		conn.close()
+		s.close()
+	except Exception as e:
+		printErr(e)
+			
+def gracefulStopHandler(signal_received, frame):
+	print('SIGINT or CTRL-C detected. Exiting gracefully')
+	gracefulStop()
+	exit(0)
+	
+def printErr(e):
+	print("Exception: %s" % str(e))
+	print("Caused by:")
+	exc_type, exc_obj, exc_tb = exc_info()
+	fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+	print(exc_type, fname, exc_tb.tb_lineno)
+	
 def genTimestamp():
 	now = datetime.now()
 	return now.strftime("%Y-%m-%d %H:%M:%S")
@@ -20,15 +47,24 @@ def genData():
 def genLine():
 	return "%s%s\r\n" % (genTimestamp(), genData())
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((HOST, PORT))
-	s.listen()
-	print("Waiting for client")
-	conn, addr = s.accept()
-	with conn:
-		print('Connected by', addr)
-		while True:
-			data = genLine()
-			print(data)
-			conn.sendall(data.encode())
-			time.sleep(1)
+if __name__ == "__main__":
+	global conn, s
+	signal(SIGINT, gracefulStopHandler)
+	
+	while True:
+		try:
+			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+				s.bind((HOST, PORT))
+				s.listen()
+				print("Waiting for client")
+				conn, addr = s.accept()
+				with conn:
+					print('Connected by', addr)
+					while True:
+						data = genLine()
+						print(data)
+						conn.sendall(data.encode())
+						time.sleep(1)
+		except Exception as e:
+			printErr(e)
+			gracefulStop()
